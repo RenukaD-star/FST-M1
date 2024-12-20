874ba86143b00a35b45acce28f8de193bb6fc437
RenukaD-star <PEPAKAYALA.Renuka.Devi@ibm.com>
2024-12-20 10:33:32 +0530

    Add files via upload

---
+++ Java/Activities/Activity5.java
@@ -0,0 +1,27 @@
+package activities;
+
+public class Activity5 {
+	public static void main(String[] args) {
+		String title = "The Shining";
+		Book newNovel = new MyBook();
+		newNovel.setTitle(title);
+		System.out.println("The title is: " + newNovel.getTitle());
+	}
+}
+	
+	abstract class Book{
+		String title;
+		abstract void setTitle(String R);
+		 public String getTitle() {
+			 return title;
+		 }
+	}
+
+		 class MyBook extends Book{
+			
+			public void setTitle(String R) {
+				title = R;
+				 
+		}
+		
+		 }
