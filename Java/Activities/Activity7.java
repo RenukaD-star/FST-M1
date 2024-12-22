e86dcbd803d28f1ec58760a3dd1312c3808d835f
RenukaD-star <PEPAKAYALA.Renuka.Devi@ibm.com>
2024-12-22 14:59:56 +0530

    Add files via upload

---
+++ Java/Activities/Activity7.java
@@ -0,0 +1,12 @@
+package activities;
+
+public class Activity7 {
+	public static void main(String[] args) {
+		MountainBike bike =new MountainBike(3,0,25);
+		System.out.println(bike.bicycleDesc());
+		bike.speedUp(20);
+		bike.applyBrake(5);
+		
+	}
+
+}
